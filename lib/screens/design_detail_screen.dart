cd6a08046d49d97f746620818c924e6aec78894d
Scorpion-005 <scorpianranger005@gmail.com>
2026-09-21 23:43:14 +0530

    Update design_detail_screen.dart

--- lib/screens/design_detail_screen.dart
+++ lib/screens/design_detail_screen.dart
@@ -25,7 +25,7 @@ class _DesignDetailScreenState extends State<DesignDetailScreen> {
     return showDialog<String>(
       context: context,
       builder: (context) => AlertDialog(
-        title: const Text('Designer Name?'),
+        title: const Text('Name of the Designer?'),
         content: TextField(
           controller: ctrl,
           autofocus: true,
